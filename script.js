9201bd37733ede536aa013cb404597d59b90e04f
omegaDeltaGamma <64288863+omegaDeltaGamma@users.noreply.github.com>
2020-10-03 23:00:36 +0100

    finally

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-let switcher = document.getElementsByClassName("input")[0];
+var switcher = document.querySelector("input");
 
 function themeSwitch() {
-    
+    currentTheme = document.documentElement.getAttribute("data-theme");
+
+    if(switcher.checked) {
+        document.documentElement.setAttribute("data-theme", "dark");
+    }
+    else {
+        document.documentElement.setAttribute("data-theme", "light")
+    }
 }
 
-switcher.addEventListener("click", themeSwitch);
+switcher.addEventListener("click", themeSwitch, false);
